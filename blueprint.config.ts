e230d3a4aa9f6ed79b96b623f177444058c89c64
Chris Chu <15345991+o-p@users.noreply.github.com>
2024-10-08 00:32:23 +0800

    :key: Support custom RPC API Key

--- blueprint.config.ts
+++ blueprint.config.ts
@@ -1,4 +1,16 @@
-export const config = {
+import { Config } from '@ton/blueprint'
+
+const customNetwork = process.env.API_KEY ? {
+  network: {
+    type: 'mainnet',
+    endpoint: 'https://toncenter.com/api/v2/jsonRPC',
+    version: 'v2',
+    key: process.env.API_KEY,
+  } as Config['network'],
+} : null
+
+export const config: Config = {
     // Compiler configurations: compilables/*.compile.ts
     separateCompilables: true,
+    ...customNetwork,
 }
